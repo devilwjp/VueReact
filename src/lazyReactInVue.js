947076f33bc73a3fd4c526e7b7a1bf69df374542
junping.wu <9700616@qq.com>
2019-11-28 18:40:17 +0800

    +++

--- src/lazyReactInVue.js
+++
@@ -1,6 +0,0 @@
-import { useReactInVue } from 'vuereact-combined'
-export default function lazyReactInVue (asyncImport) {
-  return () => asyncImport().then((mod) => {
-    return useReactInVue(mod.default)
-  })
-}
--- src/lazyVueInReact.js
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react'
-import useVueInReact from './useVueInReact'
-export default function lazyVueInReact (asyncImport) {
-  return lazy(() => asyncImport().then((mod) => {
-    return { default: useVueInReact(mod.default) }
-  }))
-}
